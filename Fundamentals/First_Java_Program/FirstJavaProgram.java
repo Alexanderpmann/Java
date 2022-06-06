d2236e2b3ef0b8dcb52dc1d385bc40c020fa5de3
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-06-06 05:31:18 -0400

    updating files

--- Fundamentals/First_Java_Program/FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-public class FirstJavaProgram { 
-    public static void main(String[] args) {
-        System.out.println("My name is Alexander P. Mann");
-        System.out.println("I am 34 years old");
-        System.out.println("My hometown is Potomac, MD");
-    }
-}
