2ec3c3448e1cd34927414e2fbfdd00259bb2fe05
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-06-09 17:20:48 -0400

    Updating Folders

---
+++ OOP/CRUD/Animals/Animal.java
@@ -0,0 +1,21 @@
+public class Animal {
+    
+
+
+public void setSpecies(String species) {
+    this.species = species;
+}
+
+public void setIsmammal(Boolean ismammal) {
+    this.ismammal = ismammal;
+}
+
+public Animal()
+{
+    name="Mighty Joe Young";
+    species="Gorilla";
+    age=8;
+    ismammal=true;
+}
+
+}
